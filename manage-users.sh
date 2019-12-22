a0caa92710e71d03206330c3aedec435cba40bda
Jorge <jmarcorb@gmail.com>
2019-12-22 13:13:25 +0100

    Update manage-users.sh

--- manage-users.sh
+++ manage-users.sh
@@ -5,10 +5,17 @@ DEVICE=$(ls /dev/sd?1)
 if [ $DEVICE ]; then
     mount "$DEVICE" "$MOUNTDIR"
     if [ $? -eq 0 ]; then
-       echo "Woohoo! Mount succeeded!"
+       echo "¡Yuhuu! ¡USB montado!"
+       cd /mnt/usb
        if [ -f "renueva-certificados.txt" ]; then
             #revocar viejos y crear nuevos 
             cd /etc/openvpn/easy-rsa
+            ./easyrsa --batch revoke llave1
+            ./easyrsa --batch revoke llave2
+            ./easyrsa --batch revoke llave3
+            ./easyrsa --batch revoke llave4
+            ./easyrsa --batch revoke llave5
+            echo "Antiguos certificados revocados (1-5)"
             #crear nuevos
             echo "############## creación de certificados ######################"
             ./easyrsa --batch --req-cn=llave1 gen-req llave1 nopass
@@ -21,16 +28,7 @@ if [ $DEVICE ]; then
             ./easyrsa --batch --days=7300 sign-req client llave4
             ./easyrsa --batch --req-cn=llave5 gen-req llave5 nopass
             ./easyrsa --batch --days=7300 sign-req client llave5
-            ./easyrsa --batch --req-cn=llave1 gen-req llave6 nopass
-            ./easyrsa --batch --days=7300 sign-req client llave6
-            ./easyrsa --batch --req-cn=llave2 gen-req llave7 nopass
-            ./easyrsa --batch --days=7300 sign-req client llave7
-            ./easyrsa --batch --req-cn=llave3 gen-req llave8 nopass
-            ./easyrsa --batch --days=7300 sign-req client llave8
-            ./easyrsa --batch --req-cn=llave4 gen-req llave9 nopass
-            ./easyrsa --batch --days=7300 sign-req client llave9
-            ./easyrsa --batch --req-cn=llave5 gen-req llave0 nopass
-            ./easyrsa --batch --days=7300 sign-req client llave0
+
             #echo "############ creación de .ovpn ######################"
             cd pki
             wget https://github.com/jmarcorb/vpnp/raw/master/defaults.txt
@@ -39,16 +37,17 @@ if [ $DEVICE ]; then
             /root/manage-vpnp.py -m llave3
             /root/manage-vpnp.py -m llave4
             /root/manage-vpnp.py -m llave5
-            /root/manage-vpnp.py -m llave6
-            /root/manage-vpnp.py -m llave7
-            /root/manage-vpnp.py -m llave8
-            /root/manage-vpnp.py -m llave9
-            /root/manage-vpnp.py -m llave0
+
+            echo "Certificados recreados (1-5). Reiniciando openvpn"
+            
+            service openvpn-server@server restart
        fi
     fi
     #copiar viejos al usb
     cd /etc/openvpn/easy-rsa/pki
     cp *.ovpn /mnt/usb/
+    sync
+    echo "Certificados copiados al USB"
 else
   echo "Crap! Mount Failed :( "
 fi
